2205d74aa63ad6fb45b52b5f404173ede4977d3b
keygenesis <david@david.rip>
2025-12-10 21:01:21 -0500

    get -> builtins

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -46,7 +46,7 @@ in
     home.file.".config/constellation_cursor/cursor.conf".text = configFile;
 
   home.sessionVariables.LD_PRELOAD =
-    "${cfg.package}/lib/libthe_constellation_cursor.so:${lib.getEnv "LD_PRELOAD"}";
+    "${cfg.package}/lib/libthe_constellation_cursor.so:${builtins.getEnv "LD_PRELOAD"}";
     
   };
 }
